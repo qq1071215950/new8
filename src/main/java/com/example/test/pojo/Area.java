38200350c8c3bf7e9e3d6d0638f79c2b8211c6bb
root123 <1071215950@qq.com>
2019-07-08 18:50:41 +0800

    add:添加用户登录的功能

---
+++ src/main/java/com/example/test/pojo/Area.java
@@ -0,0 +1,69 @@
+package com.example.test.pojo;
+
+import java.util.Date;
+
+public class Area {
+    private Integer areaId;
+    private String areaName;
+    private Integer priority;
+    private String desc;
+    private Date createTime;
+    private Date lastEditTime;
+    private String stamp;
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    public String getStamp() {
+        return stamp;
+    }
+
+    public void setStamp(String stamp) {
+        this.stamp = stamp;
+    }
+}
